c8ba4d624a71f2683e179eaec871f93c21b4ad71
Yuvaraj <113331128+rockyuva@users.noreply.github.com>
2023-08-30 11:48:57 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int sum,num1,num2;
+	printf("enter the number");
+	scanf("%d%d",&num1,num2);
+	sum=num1+num2;
+	printf("%d + %d = %d",&num1,num2,sum);
+	return 0;
+}
